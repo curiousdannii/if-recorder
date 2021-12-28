593e56a2351c602ec884ce432e460868bd4a4c20
Dannii Willis <curiousdannii@gmail.com>
2021-12-28 18:02:49 +1000

    Update for the new GlkOte design

--- tools/server/save-sql.php
+++ tools/server/save-sql.php
@@ -10,136 +10,99 @@ if( !function_exists( 'json_decode' ) ) {
     server_error( 'JSON functions are not supported by this PHP installation' );
 }
 
-if( empty( $_POST[ 'data' ] ) ) {
-	die( 'POST data not found' );
+if (isset($_POST['data'])) {
+	$data = $_POST['data'];
+}
+else {
+	$data = json_decode(file_get_contents('php://input'), true);
 }
 
-$data = $_POST[ 'data' ];
-
-if( !isset( $data[ 'session' ] ) && !isset( $data[ 0 ][ 'session' ] ) ) {
+if(!isset($data['sessionId'])) {
 	die( 'Session identifier missing' );
 }
 
-// if there is no log information, this is the start of a transcript
-if( !empty( $data[ 'start' ] ) ) {
-	if( empty( $data[ 'start' ][ 'story' ] ) ) {
-		die( 'Story identifier missing' );
-	}
-	
-	// check if the session data is already in the database, save if not
-	$query = $db->prepare(
-		"SELECT COUNT(*) as count FROM {$dbSettings[ 'prefix' ]}stories 
-			WHERE session = ?"
+$session = $data['sessionId'];
+$story = $_GET['story'];
+
+// check if the session data is already in the database, save if not
+$query = $db->prepare(
+	"SELECT COUNT(*) as count FROM {$dbSettings[ 'prefix' ]}stories 
+		WHERE session = ?"
+);
+
+$query->execute(array($session));
+$count = $query->fetchAll();
+
+if( $count[ '0' ][ 'count' ] == 0 ) { 
+	$insert = $db->prepare(
+		"INSERT INTO {$dbSettings[ 'prefix' ]}stories 
+			SET session = ?,
+			story = ?,
+			version = ?,
+			interpreter = ?,
+			browser = ?,
+			started = ?"
 	);
 	
-	$query->execute( array( $data[ 'session' ] ) );
-	$count = $query->fetchAll();
+	$interpreter = '';
+	$browser = '';
+	$storyVersion = '';
 	
-	if( $count[ '0' ][ 'count' ] == 0 ) { 
-		$insert = $db->prepare(
-			"INSERT INTO {$dbSettings[ 'prefix' ]}stories 
-				SET session = ?,
-				story = ?,
-				version = ?,
-				interpreter = ?,
-				browser = ?,
-				started = ?"
-		);
-		
-		if( empty( $data[ 'start' ][ 'interpreter' ] ) ) {
-			$interpreter = '';
-		}
-		else {
-			$interpreter = $data[ 'start' ][ 'interpreter' ];
-		}
-		
-		if( empty( $data[ 'start' ][ 'browser' ] ) ) {
-			$browser = '';
-		}
-		else {
-			$browser = $data[ 'start' ][ 'browser' ];
-		}
-		
-		if( empty( $data[ 'start' ][ 'version' ] ) ) {
-			$storyVersion = '';
-		}
-		else {
-			$storyVersion = $data[ 'start' ][ 'version' ];
-		}
-		
-		$insert->execute( 
-			array(
-				$data[ 'session' ],
-				$data[ 'start' ][ 'story' ],
-				$storyVersion,
-				$interpreter,
-				$browser,
-				date( 'Y-m-d H:i:s' )
-			)
-		) or server_error( 'Error saving startup data: '.print_r( $insert->errorInfo(), true ) );
-	}
+	$insert->execute( 
+		array(
+			$session,
+			$story,
+			$storyVersion,
+			$interpreter,
+			$browser,
+			date( 'Y-m-d H:i:s' )
+		)
+	) or server_error( 'Error saving startup data: '.print_r( $insert->errorInfo(), true ) );
 }
 
-// if there are no multiple log entries coming in at the same time,
-// create an array of the single log entry
-if( isset( $data['log'] ) ) {
-	$data = array( $data );
-}
+$timestamp = date( 'Y-m-d H:i:s', round($data['timestamp'] / 1000 ) );
+
+$insert = $db->prepare( 
+	"INSERT INTO {$dbSettings[ 'prefix' ]}transcripts 
+	SET session = ?,
+		input = ?,
+		output = ?,
+		window = ?,
+		styles = ?,
+		inputcount = ?,
+		outputcount = ?,
+		timestamp = ?"
+);
+
+$insert->execute(
+	array(
+		$session,
+		$data[ 'input' ],
+		$data[ 'output' ],
+		0,
+		'',
+		0,
+		0,
+		$timestamp
+	)
+) or server_error( 'Error saving log data: '.print_r( $insert->errorInfo(), true ) );
 
 
-/* Saving transcript pieces the database */
-foreach( $data as $d ) {
-	if( isset( $d[ 'log' ] ) && is_array( $d[ 'log' ] ) ) {
-		if( !empty( $d[ 'log' ][ 'timestamp' ] ) ) {
-			$timestamp = date( 'Y-m-d H:i:s', round( $d[ 'log' ][ 'timestamp' ] / 1000 ) );
-		}
-		else {
-			$timestamp = '';
-		}
-		
-		$insert = $db->prepare( 
-			"INSERT INTO {$dbSettings[ 'prefix' ]}transcripts 
-			SET session = ?,
-				input = ?,
-				output = ?,
-				window = ?,
-				styles = ?,
-				inputcount = ?,
-				outputcount = ?,
-				timestamp = ?"
-		);
-		
-		$insert->execute(
-			array(
-				$d[ 'session' ],
-				$d[ 'log' ][ 'input' ],
-				$d[ 'log' ][ 'output' ],
-				$d[ 'log' ][ 'window' ],
-				$d[ 'log' ][ 'styles' ],
-				$d[ 'log' ][ 'inputcount' ],
-				$d[ 'log' ][ 'outputcount' ],
-				$timestamp
-			)
-		) or server_error( 'Error saving log data: '.print_r( $insert->errorInfo(), true ) );
-		
-		
-		// Update story information. The "ended" counter is updated as transcript
-		// pieces are saved.
-		$storyupdate = $db->prepare(
-			"UPDATE {$dbSettings[ 'prefix' ]}stories 
-			SET ended = IF( ended < :timestamp, :timestamp, ended ),
-				inputcount = IF( inputcount < :count, :count, inputcount )
-			WHERE session = :session"
-		);
-		
-		$storyupdate->execute(
-			array(
-				':timestamp'	=> $timestamp,
-				':count'		=> $d[ 'log' ][ 'inputcount' ],
-				':session'		=> $d[ 'session' ]
-			)
-		) or server_error( 'Error updating story data: '.print_r( $storyupdate->errorInfo(), true ) );
-	}
-}
+// Update story information. The "ended" counter is updated as transcript
+// pieces are saved.
+$storyupdate = $db->prepare(
+	"UPDATE {$dbSettings[ 'prefix' ]}stories 
+	SET ended = IF( ended < :timestamp, :timestamp, ended ),
+		inputcount = IF( inputcount < :count, :count, inputcount )
+	WHERE session = :session"
+);
+
+$storyupdate->execute(
+	array(
+		':timestamp'	=> $timestamp,
+		':count'		=> 0,
+		':session'		=> $session
+	)
+) or server_error( 'Error updating story data: '.print_r( $storyupdate->errorInfo(), true ) );
 
 die( 'OK' );
